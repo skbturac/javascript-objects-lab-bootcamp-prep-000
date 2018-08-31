55e596c000149dbe23926bf510692f0d2670d9a3
Suheyl Turac <skbsturac@live.com>
2018-08-31 23:26:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return obj
+  var newObj = Object.assign({},object)
+  delete newObj[key];
+  return newObj
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
