790c39f77be4f4025e064aa1059956e98d61e32f
Suheyl Turac <skbsturac@live.com>
2018-08-31 23:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var recipes = {
-
-}
-
+var recipes = new Object ({})
+var obj = { prop: 1 }
 function updateObjectWithKeyAndValue(object, key, value) {
-  return
+  return Object.assign({},obj,{[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
 }
